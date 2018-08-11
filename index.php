2c0865907abe8bfaa83ad8a894daf880ffc6ab77
AlexBarbosa169 <alexsrb169@hotmail.com>
2018-08-11 18:56:54 -0300

    consulta retornando o desempenho, precisa ser testada e avaliada

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
    
    require_once '../../../config.php';
    require_once $CFG->libdir.'/gradelib.php';
+   require_once $CFG->dirroot.'/user/lib.php';
+   require_once $CFG->dirroot.'/user/profile/lib.php';
    require_once $CFG->dirroot.'/grade/lib.php';
 //   require_once $CFG->dirroot.'/grade/report/overview/lib.php';
 
@@ -18,7 +20,110 @@ $PAGE->set_heading('PluginAva');
 //imprimir cabeçalho
 echo $OUTPUT->header();
 
-echo 'Seja Bem Vindo!';
-echo "<h3>Consulta de usuários existentes no banco</h3>";
+echo "<h3>Desempenho dos estudantes do curso</h3><div class='row-fluid'>
+  <div id='1' class='span3'>Começar</div>
+  <div id='2' class='span3'>Visão Geral</div>
+  <div id='3' class='span3'>Filtro</div>
+  <div id='4' class='span3'>Dados do Aluno</div>
+</div>";
 
+$courseid = required_param('id', PARAM_INT);
+
+$usersCount = $DB->get_records_sql("select id, username from public.mdl_user");
+$bom = array();
+$medio = array();
+$ruim = array();
+
+foreach ($usersCount as $user){            
+    $teste = $DB->get_records_sql("select sum((gg.finalgrade :: bigint) * gi.aggregationcoef2)
+                                    from public.mdl_grade_items as gi 
+                                    join public.mdl_grade_grades as gg on
+                                    gi.id = gg.itemid 
+                                    join public.mdl_user as us
+                                    on gg.userid = us.id
+                                    join public.mdl_course as c on
+                                    gi.courseid = c.id
+                                    where gg.userid = us.id and c.id = $courseid 
+                                    and gi.itemtype != 'course' and us.id = $user->id");        
+    echo "Id: $user->id Nome do usuário: $user->username Desempenho: $teste->sum";
+    
+    foreach ($teste as $sum){
+        switch ($sum->sum) {
+            case NULL:
+                
+                break;
+            case $sum->sum >= 7 :
+                if($sum->sum != NULL)
+                    $bom[] = $sum->sum;
+                break;
+            case $sum->sum < 7 :
+                $ruim[] = $sum->sum;
+                break;
+            default:
+                break;
+        }
+        }
+    
+    foreach($teste as $t){        
+        echo ($t->sum * 10)."%";
+    }
+    echo "<br><br>";    
+}
+
+var_dump($bom);
+var_dump($ruim);
+$b = count($bom);
+$c = count($ruim);
+
+        echo "
+            <html>
+                <head>
+                  <script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script>
+                  <script type='text/javascript'>
+                    google.charts.load('current', {'packages':['corechart']});
+                    google.charts.setOnLoadCallback(drawChart);
+
+                    function drawChart() {
+
+                      var data = google.visualization.arrayToDataTable([
+                        ['Task', 'Hours per Day'],
+                        ['Ótimo', $b],
+                        ['Bom',      $c],                        
+                        ['Ruim', 1],                        
+                      ]);
+
+                      var options = {
+                        title: 'Desempenho dos estudantes do Curso' $curse->name,
+                            colors:['green','blue','yellow']
+                      };
+
+                      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+                        
+                            function selectHandler() {
+                                 var selectedItem = chart.getSelection()[0];
+                                if (selectedItem) {
+                                    var value = data.getValue(selectedItem.row, 0);
+                                    alert('O selecionou ver os alunos que tiveram o desempenho ' + value + '.');
+//                                      console.log(selectedItem);
+                            }
+                          }
+
+                      google.visualization.events.addListener(chart, 'select', selectHandler);
+                      chart.draw(data, options);
+                    }                                        
+                    
+                    function alteraGrid(){
+                        document.getElementById('1').style.color = 'blue';
+                    }
+                        
+                  </script>
+                </head>
+                <body>
+                  <div id='piechart' style='width: 900px; height: 500px;'></div>
+                </body>
+        </html>";
+    
+        $um = profile_load_custom_fields($currentuser);
+        var_dump($um);
+            
 echo $OUTPUT->footer();
